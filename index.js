6bef6e2eb5f0ff9aab378f151d9b4a36e641a5d1
jrc03c <jrc03c@pm.me>
2021-09-26 09:14:46 -0500

    ...

---
+++ index.js
@@ -0,0 +1,8 @@
+const DiffMatchPatch = require("./diff-match-patch/javascript/diff_match_patch_uncompressed.js")
+  .diff_match_patch
+
+function score(a, b) {
+  const dmp = new DiffMatchPatch()
+  const diff = dmp.diff_main(a, b)
+  return dmp.diff_levenshtein(diff) / Math.max(a.length, b.length)
+}
